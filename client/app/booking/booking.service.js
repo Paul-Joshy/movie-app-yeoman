1bdf2c7bb7ed250a81d83756e70279abbd6acf69
Paul Joshy <pauljoshyk96@gmail.com>
2017-05-09 21:59:03 +0530

    made booking route work

--- client/app/booking/booking.service.js
+++ client/app/booking/booking.service.js
@@ -6,6 +6,9 @@ function bookingService() {
   movieDetails.name = "";
   movieDetails.date = "";
   movieDetails.time = "";
+  // movieDetails.bookedSeats = [];
+  movieDetails.selectedSeats = [];
+  movieDetails.grandTotal = ""
 
   this.setName = function(name){
     movieDetails.name = name;
@@ -15,10 +18,16 @@ function bookingService() {
     movieDetails.date = date;
     movieDetails.time = time;
   }
-  // 
+
   // this.setTime = function(time){
   //   movieDetails.time = time;
   // }
+
+  this.addSelected = function(selected, grandTotal){
+    // movieDetails.bookedSeats = movieDetails.bookedSeats.concat(selected);
+    movieDetails.selectedSeats = selected;
+    movieDetails.grandTotal = grandTotal;
+  }
 
   this.getDetails = function(){
     return movieDetails;
--- client/app/seating/seating.controller.js
+++ client/app/seating/seating.controller.js
@@ -3,16 +3,74 @@
 (function(){
 
 class SeatingComponent {
-  constructor($scope, $http, socket, booking) {
+  constructor($scope, $http, socket, booking, $location) {
     this.message = 'Hello';
     this.$scope = $scope;
     this.$http = $http;
     this.bookingService = booking;
+    this.rows = ['A','B','C','D','E','F'];
+    this.columns = [1,2,3,4,5,6,7,8,9,10];
+    // this.selected = false;
+    this.selectedSeats = [];
+    this.bookedSeats = [];
+    this.bookingForm = {};
+    this.$location = $location;
   }
 
   $onInit(){
     var movieDetails = this.bookingService.getDetails();
     console.log(movieDetails);
+    console.log(this.rows);
+    console.log(this.columns);
+    // this.bookedSeats = movieDetails.bookedSeats;
+    console.log(this.bookedSeats);
+  }
+
+  isSelected(row, col){
+    if(_.find(this.selectedSeats, function(seat){ return seat.row === row && seat.col === col })){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isBooked(row, col){
+    if(_.find(this.bookedSeats, function(seat){ return seat.row === row && seat.col === col})){
+        return true;
+    } else{
+      return false;
+    }
+  }
+
+  selectSeat(row, col, classType){
+    if(!this.isSelected(row, col) && !this.isBooked(row, col)){
+      console.log("selected")
+      this.selectedSeats.push({
+        row: row,
+        col: col,
+        classType: classType
+      });
+    }
+
+    else{
+      this.selectedSeats = _.reject(this.selectedSeats, function(seat){ return seat.row === row && seat.col===col })
+    }
+
+    this.bookingForm.gold = _.filter(this.selectedSeats, function(seat){ return seat.classType === "gold" });
+    this.bookingForm.silver = _.filter(this.selectedSeats, function(seat){ return seat.classType === "silver"})
+    this.bookingForm.grandTotal = ( (this.bookingForm.gold.length * 200) + (this.bookingForm.silver.length * 100) + 30)
+    console.log(this.bookingForm);
+    console.log(this.selectedSeats);
+  }
+
+  bookSeats(){
+    console.log("shoop baby");
+    this.bookingService.addSelected(this.selectedSeats, this.bookingForm.grandTotal);
+    console.log(this.bookingService.getDetails());
+    var movieDetails = this.bookingService.getDetails();
+    this.bookedSeats = movieDetails.bookedSeats;
+    this.selectedSeats = [];
+    this.$location.path('/payments');
   }
 }
 
